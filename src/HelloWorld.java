1187ddfd15e82315f5b61ae44db79124defbf70c
Boonem <mcboone228@gmail.com>
2024-02-19 19:44:47 -0500

    adding a word or two

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
-		System.out.println("Hello again alternate!");
+		System.out.println("A word or two: Hello again alternate!");
 	}
 }
