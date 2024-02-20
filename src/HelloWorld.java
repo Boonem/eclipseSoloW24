0a3faedb3733f6bdd4d0a663e5749b6a67bc938c
Boonem <mcboone228@gmail.com>
2024-02-19 19:49:16 -0500

    Making changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
-		System.out.println("Hello again alternate!");
+		System.out.println("Hello again alternate! We're making some changes.");
 	}
 }
